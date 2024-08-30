c30047748cee55a47554ed11995590a100306255
AndrewKuprikov <andrewkub03@gmail.com>
2024-08-30 23:13:12 +0300

    Section 2 - props & events demo

---
+++ demo/src/demo1/DemoCounter.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="counter">
+    <counter-button @action="decrement"> - </counter-button>
+
+    <counter-value :value="count"></counter-value>
+
+    <counter-button @action="increment"> + </counter-button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import CounterButton from './CounterButton.vue'
+import CounterValue from './CounterValue.vue'
+
+const count = ref(0)
+
+/* Метод уменьшения значения */
+const decrement = () => {
+  count.value -= 1
+}
+
+/* Метод увеличения значения */
+const increment = () => {
+  count.value += 1
+}
+</script>
+
+<style lang="scss" scoped>
+.counter {
+  display: flex;
+  align-items: center;
+}
+</style>
